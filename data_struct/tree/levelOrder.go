93d7caef45bf2b864896f591392b45f1d2b2bdf7
jinrui <jinrui@jinruideMacBook-Pro.local>
2020-08-12 15:50:59 +0800

    tree

---
+++ data_struct/tree/levelOrder.go
@@ -0,0 +1,31 @@
+package tree
+
+func levelOrder(root *TreeNode) [][]int {
+	output := [][]int{}
+	if root == nil {
+		return output
+	}
+
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		count := len(queue)
+		level := make([]int, 0, count)
+
+		for count > 0 {
+			node := queue[0]
+			queue = queue[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		output = append(output, level)
+	}
+
+	return output
+}
